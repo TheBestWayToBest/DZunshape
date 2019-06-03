b3bfaba1e38e7eb4881215d91ea442b8929387cf
TheBestWayToBest <41147305+TheBestWayToBest@users.noreply.github.com>
2019-06-03 11:42:49 +0800

    更新

---
+++ Business/Modle/TaskDetail.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Modle
+{
+    public class TaskDetail
+    {
+        /// <summary>
+        /// 户序
+        /// </summary>
+        public decimal? SortSeq { get; set; }
+        /// <summary>
+        /// 分拣任务号
+        /// </summary>
+        public decimal? SortNum { get; set; }
+        /// <summary>
+        /// 车组号
+        /// </summary>
+        public string RegionCode { get; set; }
+        /// <summary>
+        /// 卷烟编号
+        /// </summary>
+        public string CigCode { get; set; }
+        /// <summary>
+        /// 客户名称
+        /// </summary>
+        public string CusName { get; set; }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public decimal? Status { get; set; }
+        /// <summary>
+        /// 通道号
+        /// </summary>
+        public string ThroughNum { get; set; }
+        /// <summary>
+        /// 订单编号
+        /// </summary>
+        public string BillCode { get; set; }
+        /// <summary>
+        /// 卷烟名称
+        /// </summary>
+        public string CigName { get; set; }
+        /// <summary>
+        /// 抓烟数量
+        /// </summary>
+        public decimal Num { get; set; }
+    }
+}
--- SortingControlSys/StatusManager.cs
+++ SortingControlSys/StatusManager.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Business.BusinessClass;
+using Business.Modle;
 
 namespace SortingControlSys
 {
@@ -19,6 +21,52 @@ namespace SortingControlSys
 
         private void BtnSearch_Click(object sender, EventArgs e)
         {
+            int sortNum = 0;
+            bool a = int.TryParse(TxtSortNum.Text, out sortNum);
+            List<TaskDetail> list = UnPokeClass.GetDataAll(1, sortNum, TxtRegionCode.Text, TxtCusID.Text);
+            foreach (var item in list)
+            {
+                DataGridViewCellStyle dgvStyle = new DataGridViewCellStyle();
+                dgvStyle.BackColor = Color.LightGreen;
+                // 存了状态值  
+                string status = "";
+                int index = this.DgvData.Rows.Add();
+                this.DgvData.Rows[index].Cells[0].Value = item.SortSeq;//户序
+                this.DgvData.Rows[index].Cells[1].Value = item.SortNum; //分拣任务号
+
+                this.DgvData.Rows[index].Cells[2].Value = item.RegionCode;//车组号
+                this.DgvData.Rows[index].Cells[3].Value = item.CusName;//客户名称
+                this.DgvData.Rows[index].Cells[4].Value = item.CigCode;//香烟编号
+                this.DgvData.Rows[index].Cells[5].Value = item.CigName;//香烟名称
+                //this.DgvData.Rows[index].Cells[8].Value = item.POKENUM;//抓烟数量
+                this.DgvData.Rows[index].Cells[6].Value = item.Status;//状态位
+                this.DgvData.Rows[index].Cells[7].Value = item.ThroughNum;//物理通道号
+                this.DgvData.Rows[index].Cells[8].Value = item.BillCode;//订单号
+
+                if (item.Status == 10)
+                {
+                    status = "新增";
+                }
+                else if (item.Status == 12)
+                {
+                    status = "已计算";
+                }
+                else if (item.Status == 15)
+                {
+                    status = "已发送";
+                }
+                else
+                {
+                    status = "完成";
+                }
+                this.DgvData.Rows[index].Cells[9].Value = status;//状态位
+
+                if (status == "完成")
+                {
+                    this.DgvData.Rows[index].Cells[9].Style = dgvStyle;
+
+                }
+            }
 
         }
 
