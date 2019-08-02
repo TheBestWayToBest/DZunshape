8662c7561299712022971e54aa199c2746ae2acc
nj <247596999@qq.com>
2019-08-02 11:20:10 +0800

    Update Replan.cs

--- Business/BusinessClass/Replan.cs
+++ Business/BusinessClass/Replan.cs
@@ -21,8 +21,8 @@ namespace Business.BusinessClass
                     var sortList = (from item in entites.T_PRODUCE_SORTTROUGH where item.TROUGHTYPE == 10 && item.STATE == "10" && (item.CIGARETTETYPE == 30 || item.CIGARETTETYPE == 40) select item).ToList();
                     sortList.ForEach(x => x.LASTMANTISSA = x.MANTISSA);//更新上次尾数
                     var pokeList1 = (from item in entites.T_UN_POKE where item.STATUS == 0 select item).ToList();
-                    var pokeList = (from item in entites.T_UN_POKE where item.STATUS == 0 group item by new { item.SORTNUM, item.TROUGHNUM, item.CIGARETTECODE } into g select new { SORTNUM = g.Key.SORTNUM, TROUGHNUM = g.Key.TROUGHNUM, CIGARETTECODE = g.Key.CIGARETTECODE, POKENUM = g.Sum(x=>x.POKENUM) }).OrderBy(x => x.SORTNUM).OrderBy(x => x.TROUGHNUM).ToList();
-                    var itemList = (from item in entites.T_WMS_ITEM select item).ToList();
+                    var pokeList = (from item in entites.T_UN_POKE where item.STATUS == 0 group item by new { item.SORTNUM, item.TROUGHNUM, item.CIGARETTECODE } into g orderby g.Key.SORTNUM,g.Key.TROUGHNUM select new { SORTNUM = g.Key.SORTNUM, TROUGHNUM = g.Key.TROUGHNUM, CIGARETTECODE = g.Key.CIGARETTECODE, POKENUM = g.Sum(x=>x.POKENUM) }).ToList();
+                    var itemList = (from item in entites.T_WMS_ITEM where item.ROWSTATUS==10 select item).ToList();
                     var list = (from item in entites.T_PRODUCE_REPLENISHPLAN select item.ID).ToList();
                     decimal maxid = 0;
                     if (list.Count > 0)
@@ -33,7 +33,7 @@ namespace Business.BusinessClass
                     {
                         foreach (var item in pokeList)
                         {
-                            T_PRODUCE_SORTTROUGH sorttrough = sortList.Find(x => x.CIGARETTECODE == item.CIGARETTECODE);
+                            T_PRODUCE_SORTTROUGH sorttrough = sortList.Find(x => x.CIGARETTECODE == item.CIGARETTECODE && x.TROUGHNUM==item.TROUGHNUM);
 
                             T_WMS_ITEM citem = itemList.Find(x => x.ITEMNO == item.CIGARETTECODE);
                             if (sorttrough.THRESHOLD - sorttrough.MANTISSA >= citem.JT_SIZE)//如果空出一件烟就可以补
@@ -46,7 +46,7 @@ namespace Business.BusinessClass
                                 plan.ID = (++maxid);
                                 plan.JYCODE = citem.BIGBOX_BAR;
                                 plan.REPLENISHQTY = 1;
-                                plan.TASKNUM = plan.ID + "";
+                                plan.TASKNUM = item.SORTNUM+"";
                                 plan.MANTISSA = sorttrough.MANTISSA + citem.JT_SIZE;
                                 sorttrough.MANTISSA = plan.MANTISSA;
                                 plan.TROUGHNUM = sorttrough.MACHINESEQ.ToString();//通道编号默认
